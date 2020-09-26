d7965de7021ef18926aa91c71706d0f0036c1ca2
matthijs <matthijs@ramlab.com>
2020-09-26 10:16:56 +0200

    make more info to the pypi web page

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import json
-import os
+from os import path
 from setuptools import setup
 
 
 with open('package.json') as f:
     package = json.load(f)
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
@@ -16,6 +20,9 @@ setup(
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/matthijsramlab/dash_tour_component",
     install_requires=[],
     classifiers = [
         'Framework :: Dash',

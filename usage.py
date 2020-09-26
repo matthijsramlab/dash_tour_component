2a94e29b755e6c7db6641beb0d907bcee5bff25e
matthijs <matthijs@ramlab.com>
2020-09-26 10:16:50 +0200

    make more info to the pypi web page

--- usage.py
+++ usage.py
@@ -6,30 +6,57 @@ import dash_core_components as dcc
 
 app = dash.Dash(__name__)
 
-
 app.layout = html.Div([
 	dash_tour_component.DashTour(
 		steps=[
 			{
 				'selector': '[id="my_first_step"]',
-				'content': html.Div("test"),
+				'content': "This is my first step",
+				# 'position': "center"
 			},
 			{
 				'selector': '[id="my_second_step"]',
-				'content': 'This is my second Step',
+				'content': '',  # content empty updated in callback
 			}
 		],
-		isOpen=True
+		isOpen=False,
+		id="tour_component",
+		children=html.Div()
 	),
-	html.Div("Test 1", id='my_first_step'),
-	html.Div(style={'height': '400px'}),
-	html.Div("Test 2", id='my_second_step')
+	html.Button("Open Tour", id='open_tour_button'),
+	html.Div("Test 1", id='my_first_step', style={'text-align': 'center'}),
+	html.Div(style={'height': '200px'}),
+	html.Div("Test 2", id='my_second_step', style={'text-align': 'center'}),
+	html.Br(),
+	html.Div(id='current_step_div', style={'text-align': 'center'})
 ])
 
 
-# @app.callback(Output('output', 'children'), [Input('input', 'value')])
-# def display_output(value):
-# 	return 'You have entered {}'.format(value)
+@app.callback(
+	Output('tour_component', 'isOpen'),
+	[Input('open_tour_button', 'n_clicks')],
+	prevent_initial_call=True
+)
+def open_tour_component(value):
+	return True
+
+
+@app.callback(
+	Output('current_step_div', 'children'),
+	Output('tour_component', 'children'),
+	Input('tour_component', 'CurrentStep'),
+	prevent_initial_call=True
+)
+def on_current_step_change(step):
+	# Override the content at step two
+	if step == 2:
+		content = html.Div([
+			dcc.Input(placeholder='some random input')
+		])
+	else:
+		content = ""
+
+	return html.H3(f'The current step is {step}'), content
 
 
 if __name__ == '__main__':
